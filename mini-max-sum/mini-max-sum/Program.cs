67dd61e15b6804b2f53881eef6fdb50454390c70
Douglas Lima <Douglasproglima@gmail.com>
2022-01-08 15:59:32 -0300

    First resoluction the mini-max-sum algorithm

---
+++ mini-max-sum/mini-max-sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mini_max_sum
+{
+    internal class Program
+    {
+        public static void miniMaxSum(List<int> numbers)
+        {
+            int minimumNumber = 0; 
+            minimumNumber = numbers.Min();
+
+            int maximumNumber = 0; 
+            maximumNumber = numbers.Max();
+
+            List<int> maximumNumberTemp = new List<int>(numbers);
+            maximumNumberTemp.Remove(minimumNumber);
+
+            int maximumTotal = maximumNumberTemp.Sum();
+
+            List<int> minimumNumberTemp = new List<int>(numbers);
+            minimumNumberTemp.Remove(maximumNumber);
+
+            int minimumTotal = minimumNumberTemp.Sum();
+
+            Console.WriteLine(minimumTotal + " " + maximumTotal);
+        }
+
+        static void Main(string[] args)
+        {
+            string result = Console.ReadLine().TrimEnd();
+            List<string> resultList = result.Split(' ').ToList();
+            List<int> numbers = resultList.Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            miniMaxSum(numbers);
+        }
+    }
+}
