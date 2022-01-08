5060019571d7289615a2089c8d990b0932cbda96
Douglas Lima <douglasproglima@gmail.com>
2022-01-08 12:27:21 -0300

    Resolved

---
+++ StairCase/StairCase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace StairCase
+{
+    internal class Program
+    {
+        public static void staircase(int size)
+        {
+            int level = size - 1;
+            for (int indiceA = 1; indiceA <= size; indiceA++)
+            {
+                StringBuilder str = new StringBuilder();
+
+                for (int indiceB = 0; indiceB < level; indiceB++)
+                {
+                    str.Append(" ");
+                }
+
+                for (int indiceB = 0; indiceB < size - level; indiceB++)
+                {
+                    str.Append("#");
+                }
+
+                level -= 1;
+                Console.WriteLine(str.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine().Trim());
+
+            staircase(size);
+        }
+    }
+}
