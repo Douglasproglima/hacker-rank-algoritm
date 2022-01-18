9c9ac359b42f3ee07627ca57d5132bfd4f89461f
Douglas Lima <Douglasproglima@gmail.com>
2022-01-17 21:29:53 -0300

    Finish Resoluction

--- grading-student/grading-student/Program.cs
+++ grading-student/grading-student/Program.cs
@@ -7,38 +7,38 @@ namespace grading_student
     {
         public static List<int> gradingStudents(List<int> grades)
         {
+
             for (int index = 0; index < grades.Count; index++)
             {
-                var item = grades[index];
-                if (item >= 38)
-                {
-                    var diff = 5 - (item % 5);
-                    if (diff < 3) grades[index] = item + diff;
-                }
+                var grade = grades[index];
+                int next = ((grade / 5) + 1) * 5;
+
+                if (next - grade >= 3 || grade < 38)
+                    grades[index] = grade;
+                else
+                    grades[index] = next;
             }
 
             return grades;
-        }
+            }
 
         static void Main(string[] args)
         {
-            //int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
 
             List<int> grades = new List<int>();
 
-            /*
             for (int i = 0; i < gradesCount; i++)
             {
                 int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
                 grades.Add(gradesItem);
             }
-            */
 
-            grades.Add(4);
-            grades.Add(73);
-            grades.Add(67);
-            grades.Add(38);
-            grades.Add(33);
+            //grades.Add(4);
+            //grades.Add(73);
+            //grades.Add(67);
+            //grades.Add(38);
+            //grades.Add(33);
 
             List<int> result = gradingStudents(grades);
 
